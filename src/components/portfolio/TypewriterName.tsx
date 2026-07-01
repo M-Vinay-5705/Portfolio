dd691cf817025e099931da921b2f1b01d8d9f648
Moningi Vinay Kumar <MONINGIVINAYKUMAR5705@GMAIL.COM>
2026-07-01 18:04:20 +0530

    for resopnsiveness 2

--- src/components/portfolio/TypewriterName.tsx
+++ src/components/portfolio/TypewriterName.tsx
@@ -14,8 +14,8 @@ const TypewriterName = ({ text }: TypewriterNameProps) => {
   }, [index, text]);
 
   return (
-    <span className="inline-flex max-w-full flex-wrap items-center break-words">
-      <span>{text.slice(0, index)}</span>
+    <span className="break-words">
+      {text.slice(0, index)}
       <span aria-hidden="true" className="ml-1 inline-block h-[1em] w-px bg-primary align-middle animate-caret" />
     </span>
   );
